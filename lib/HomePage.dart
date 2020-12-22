e69f306395cbf2396f702c2e76f51f80d61a512a
Ayush13Bhatia <ayushbhatia668@gmail.com>
2020-12-22 20:24:00 +0530

    HomePage.dart

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,128 +1,70 @@
-import 'package:flutter/material.dart';
-
 import 'dart:math';
 
+import 'package:flutter/material.dart';
+
 class HomePage extends StatefulWidget {
+  // AssetImage
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  AssetImage one = AssetImage('images/one.png');
-  AssetImage two = AssetImage('images/two.png');
-  AssetImage three = AssetImage('images/three.png');
-  AssetImage four = AssetImage('images/four.png');
-  AssetImage five = AssetImage('images/five.png');
-  AssetImage six = AssetImage('images/six.png');
-
-  AssetImage diceimageone;
-  AssetImage diceimagetwo;
-
-  @override
-  void initState() {
-    super.initState();
+  var imageArray = [
+    'one.png',
+    'two.png',
+    'three.png',
+    'four.png',
+    'five.png',
+    'six.png',
+  ];
+  int randomIntForDiceOne = Random().nextInt(6);
+  int randomIntForDiceTwo = Random().nextInt(6);
+  void changeImage() {
     setState(() {
-      diceimageone = one;
-      diceimagetwo = two;
-    });
-  }
-
-  void rollDices() {
-    rollDiceone();
-    rollDicetwo();
-  }
-
-  void rollDiceone() {
-    int randomfordiceone = (1 + Random().nextInt(6));
-
-    AssetImage newImage;
-
-    switch (randomfordiceone) {
-      case 1:
-        newImage = one;
-        break;
-      case 2:
-        newImage = two;
-        break;
-      case 3:
-        newImage = three;
-        break;
-      case 4:
-        newImage = four;
-        break;
-      case 5:
-        newImage = five;
-        break;
-      case 6:
-        newImage = six;
-        break;
-      default:
-    }
-    setState(() {
-      diceimageone = newImage;
-    });
-  }
-
-  void rollDicetwo() {
-    int randomfordicetwo = (1 + Random().nextInt(6));
-
-    AssetImage newImage;
-
-    switch (randomfordicetwo) {
-      case 1:
-        newImage = one;
-        break;
-      case 2:
-        newImage = two;
-        break;
-      case 3:
-        newImage = three;
-        break;
-      case 4:
-        newImage = four;
-        break;
-      case 5:
-        newImage = five;
-        break;
-      case 6:
-        newImage = six;
-        break;
-      default:
-    }
-    setState(() {
-      diceimagetwo = newImage;
+      randomIntForDiceOne = Random().nextInt(6);
+      randomIntForDiceTwo = Random().nextInt(6);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Dice Roller'),
-      ),
-      body: Container(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Image(image: diceimageone, width: 150.0, height: 150.0),
-              Image(image: diceimagetwo, width: 150.0, height: 150.0),
-              Container(
-                margin: EdgeInsets.only(top: 100.0),
-                child: RaisedButton(
-                  color: Color(0X28015A),
-                  padding: EdgeInsets.fromLTRB(30.0, 15.0, 30.0, 15.0),
-                  child: Text('Roll the dice!'),
-                  onPressed: rollDices,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(40.0),
-                  ),
-                ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30, 50, 30, 50),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 190),
+              child: Text(
+                "The sum is: " +
+                    (randomIntForDiceOne + randomIntForDiceTwo + 2).toString(),
+                style: TextStyle(fontStyle: FontStyle.italic, fontSize: 20),
               ),
-            ],
-          ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset("images/" + imageArray[randomIntForDiceOne],
+                    height: 150, width: 150),
+                Image.asset("images/" + imageArray[randomIntForDiceTwo],
+                    height: 150, width: 150),
+              ],
+            ),
+          ],
         ),
       ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Container(
+          height: 70.0,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: changeImage,
+        child: Icon(Icons.add),
+        // Text((randomIntForDiceOne + randomIntForDiceTwo).toString()),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
